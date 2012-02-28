b73c84436dee902ef21c61e8832a55e3cde0e3ad
alagu <allagappan@gmail.com>
2012-02-28 23:22:41 +0530

    Do not use Mechanize's xml parser. use xml-simple

--- song_downloader.rb
+++ song_downloader.rb
@@ -1,14 +1,7 @@
 require 'rubygems'
 require 'mechanize'
 require 'xmlsimple'
-  
-class XmlParser < Mechanize::File
-  attr_reader :xml
-  def initialize(uri = nil, response = nil, body = nil, code = nil)
-    @xml = Nokogiri::XML(body)
-    super uri, response, body, code
-  end
-end
+require 'uri'
 
 class SongDownloader
   def initialize(song_id)
@@ -18,17 +11,25 @@ class SongDownloader
     }
   end
   
-  def get_xml_id (player_content)
+  def get_playlist_id (player_content)
     script    = player_content.search('//script')[2]
     xml_line  = script.text.split("\n")[2]
-    xml_id    = xml_line.split('"')[3]
+    playlist_id    = xml_line.split('"')[3]
   end
   
-  def download_xml (xml_id)
-    xml_url  = "http://thiraipaadal.com/plists/#{xml_id}.xml?767"
+  def get_song_path (playlist_id)
+    xml_url  = "http://thiraipaadal.com/plists/#{playlist_id}.xml?767"
     response = @agent.get(xml_url)
     xml      = XmlSimple.xml_in response.body.gsub('&', ' ')
     return xml["trackList"][0]["track"].first["location"].first
+  end
+  
+  def download_url (song_path)
+    song_name = song_path.split(' -- ')[1] + '.mp3'
+    song_name = URI.escape song_name
+    folder_name = song_path.split(' - ')[0]
+    
+    puts "http://thiraipaadal.com/tempdownloads/084097109105108/7779867369657666857783/#{folder_name}/#{song_name}"
   end
   
   def download_player_page
@@ -37,10 +38,10 @@ class SongDownloader
   end
   
   def download
-    xml_id = get_xml_id(download_player_page)
+    playlist_id = get_playlist_id(download_player_page)
     
-    puts "Got xml id #{xml_id}"
+    puts "Got xml id #{playlist_id}"
     
-    download_xml(xml_id)
+    song_path = get_song_path(playlist_id)
   end
 end
