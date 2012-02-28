3799d873def9870a20fda9b5a2d1492a71b877dc
alagu <allagappan@gmail.com>
2012-02-28 22:07:07 +0530

    Raise error when no songs found

--- tamilmp3.rb
+++ tamilmp3.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'mechanize'
 require 'pp'
 
-@agent = Mechanize.new
+@agent = Mechanize.new { |agent|
+  agent.user_agent_alias = 'Mac Safari'
+}
 
 
 class SongDownload
@@ -11,12 +13,32 @@ class SongDownload
     @agent = Mechanize.new
   end
   
-  def fetch_tables
+  def parse_songs (songs)
+    songs_parsed = []
+    songs.each do |song|
+      zero_results = (song.text.index('zero results'))
+      
+      pp zero_results
+      
+      if zero_results
+        raise "No results found for #{@query}"
+      end
+      song_hash = {:name => song.children[2].text,
+                   :singers => song.children[2].text,
+                   :id => song.children[0].children[0][:value]
+                   }
+      songs_parsed.push song_hash
+    end
+    
+    songs_parsed
+  end
+  
+  def search_songs
     page = fetch_page
     rowA = page.search("//tr[@class='rowA']")
     rowB = page.search("//tr[@class='rowB']")
     
-    rowA + rowB
+    parse_songs (rowA[1..rowA.length] + rowB[1..rowB.length])
   end
 
   def fetch_page
