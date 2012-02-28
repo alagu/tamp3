e2bc5177d0e35715b2846a108e11c60e0d86a9fa
alagu <allagappan@gmail.com>
2012-02-28 23:42:01 +0530

    Downloader does progressive status update

--- song_downloader.rb
+++ song_downloader.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'mechanize'
 require 'xmlsimple'
 require 'uri'
+require 'net/http'
 
 class SongDownloader
-  def initialize(song_id)
+  def initialize(song_id, song_name)
     @song_id = song_id
+    @song_name = song_name
     @agent = Mechanize.new { |agent|
       agent.user_agent_alias = 'Mac Safari'
     }
@@ -29,7 +31,7 @@ class SongDownloader
     song_name = URI.escape song_name
     folder_name = song_path.split(' - ')[0]
     
-    puts "http://thiraipaadal.com/tempdownloads/084097109105108/7779867369657666857783/#{folder_name}/#{song_name}"
+    "http://thiraipaadal.com/tempdownloads/084097109105108/7779867369657666857783/#{folder_name}/#{song_name}"
   end
   
   def download_player_page
@@ -37,11 +39,38 @@ class SongDownloader
     @agent.get(player_url)
   end
   
+  def fetch_mp3 (url)
+    Thread.new do
+      thread = Thread.current
+      body = thread[:body] = []
+    
+      url = URI.parse url
+      Net::HTTP.new(url.host, url.port).request_get(url.path) do |response|
+        length = thread[:length] = response['Content-Length'].to_i
+    
+        File.open @song_name + '.mp3', 'w' do |io|          
+          response.read_body do |fragment|
+            io.write << fragment
+            thread[:done] = (thread[:done] || 0) + fragment.length
+            thread[:progress] = thread[:done].quo(length) * 100
+          end
+        end
+      end
+    end
+  end
+  
   def download
     playlist_id = get_playlist_id(download_player_page)
     
-    puts "Got xml id #{playlist_id}"
-    
+    puts " "
+    puts "Got xml id (#{playlist_id})"    
     song_path = get_song_path(playlist_id)
+
+    download_link =  download_url(song_path)
+    puts "Got song download link #{download_link}"
+    
+    thread = fetch_mp3(download_link)
+    puts "%.2f%%" % thread[:progress].to_f until thread.join 1
+    
   end
 end
