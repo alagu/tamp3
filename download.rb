46f2650a599de9083acf487a86c208968cd7f280
alagu <allagappan@gmail.com>
2012-02-28 22:15:42 +0530

    Formatted output

--- download.rb
+++ download.rb
@@ -3,5 +3,4 @@ require './tamilmp3.rb'
 
 song_name = ARGV.join "+"
 song_download = SongDownload.new song_name
-songs = song_download.search_songs
-pp songs
+songs = song_download.search_and_print_songs
--- tamilmp3.rb
+++ tamilmp3.rb
@@ -18,13 +18,13 @@ class SongDownload
     songs.each do |song|
       zero_results = (song.text.index('zero results'))
       
-      pp zero_results
-      
-      if zero_results
-        raise "No results found for #{@query}"
+      if not zero_results.nil?
+        return []
       end
-      song_hash = {:name => song.children[2].text,
-                   :singers => song.children[2].text,
+      
+      song_hash = {:movie => song.children[1].text,
+                   :name => song.children[2].text,
+                   :singers => song.children[3].text,
                    :id => song.children[0].children[0][:value]
                    }
       songs_parsed.push song_hash
@@ -40,12 +40,27 @@ class SongDownload
     
     parse_songs (rowA[1..rowA.length] + rowB[1..rowB.length])
   end
+  
+  def search_and_print_songs
+    songs = search_songs
+    songs.each_with_index do |song,index|
+      puts "#{index+1}. #{song[:name]}"
+      puts "   Movie: #{song[:movie]}"
+      puts "   Singers: #{song[:singers]}"
+      puts " "
+    end
+    
+    return songs
+  end
 
   def fetch_page
     url = "http://thiraipaadal.com/search.php?txtselected=&lang=en&schMovieName=&schSongName=#{@song_term}&schSinger=&schLyrics=No&sbt=++Search++"
     
+    puts "Hitting #{url}"
+    
     page = @agent.get(url)
     return page
   end
+  
 end
 
