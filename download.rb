1044699e59fb42c2a0b5f3b34425aded95434f76
alagu <allagappan@gmail.com>
2012-02-28 23:41:47 +0530

    Download selected file

--- download.rb
+++ download.rb
@@ -11,9 +11,12 @@ songs = song_search.search_and_print_songs
 
 if songs.length > 0
   print "Which one to download (1-#{songs.length}) ? "
-  index   = STDIN.gets 
-  index   = index.chomp.to_i
-  song_id = songs[index-1][:id]
+  index         = STDIN.gets 
+  index         = index.chomp.to_i
+  selected_song = songs[index-1]
+  song_id       = selected_song[:id]
+  song_name     = selected_song[:name]
   
-  song_downloader = SongDownloader.new song_id
+  song_downloader = SongDownloader.new(song_id, song_name)
+  song_downloader.download
 end
