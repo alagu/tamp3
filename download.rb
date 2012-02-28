2a2a13b3c8a6882c407eae40654cf3ef7470f7ad
alagu <allagappan@gmail.com>
2012-02-28 22:23:56 +0530

    Fetch which song to download

--- download.rb
+++ download.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
+require 'rubygems'
+require 'mechanize'
+require 'pp'
 require './tamilmp3.rb'
 
 song_name = ARGV.join "+"
-song_download = SongDownload.new song_name
+song_search = SongSearch.new song_name
 songs = song_download.search_and_print_songs
+
+if songs.length > 0
+  print "Which one to download (1-#{songs.length}) ? "
+  index   = STDIN.gets 
+  index   = index.chomp.to_i
+  song_id = songs[index-1][:id]
+  
+
+end
